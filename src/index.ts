64d2734bd1dfe522010cf6b434436f7cfd558add
Mark Lyck <mark.lyck@gmail.com>
2023-03-15 18:28:45 +0100

    :alembic: test Pr

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import { Probot } from "probot";
 
 export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
-    console.log("🔈 ~ context:", context);
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    await context.octokit.issues.createComment(issueComment);
-  });
+  // app.on("issues.opened", async (context) => {
+  //   console.log("🔈 ~ context:", context);
+  //   const issueComment = context.issue({
+  //     body: "Thanks for opening this issue!",
+  //   });
+  //   await context.octokit.issues.createComment(issueComment);
+  // });
   app.on(
     ["pull_request.opened", "pull_request.synchronize"],
     async (context) => {
+      console.log("TEST");
       console.log("🔈 ~ context:", context);
 
       const prComment = context.issue({
